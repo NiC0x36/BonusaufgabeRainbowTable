ba2e1299bd2d9c19716e9df8906df550d765a37b
Nicolas <nicolas.novak@students.fhnw.ch>
2017-05-16 16:32:03 +0200

    Initial Commit

---
+++ src/Bonusaufgabe.java
@@ -0,0 +1,28 @@
+public class Bonusaufgabe {
+	// TODO: make some table here
+
+	public static void main(String[] args) {
+		// TODO: Write the main method
+	}
+
+	private void findPlaintext(String hash) {
+		// TODO Write the method to find a plaintext with given hash
+
+	}
+
+	private void createRainbowTable() {
+		// TODO Write the rainbow table creation method
+
+	}
+
+	private String hashfunction(String plaintext) {
+		// TODO Write MD-5 Hashfunction
+		return null;
+
+	}
+
+	private String reductionfunction(String hash) {
+		// TODO Write reduction function
+		return null;
+	}
+}
