b215d841e408d13ab3e2cc6202d41c2de02b4747
dombaer <dom.baer@hispeed.ch>
2017-05-28 20:45:46 +0200

    added comments to code

--- src/Bonusaufgabe.java
+++ src/Bonusaufgabe.java
@@ -7,7 +7,7 @@ import java.security.*;
 public class Bonusaufgabe {
 	private static int N = 2000;
 	private static int K = 2000;
-	private static int L = 7; // Länge des Passworts
+	private static int L = 7; // Lï¿½nge des Passworts
 	private static char[] Z = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 	// k -> endpunkt / v -> startpunkt
@@ -15,7 +15,10 @@ public class Bonusaufgabe {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
+
+		// Timer (in ms) to see how long the creation of the rainbowtable took
 		long start = System.currentTimeMillis();
+		// creating the rainbowtable
 		createRainbowTable();
 		long duration = System.currentTimeMillis() - start;
 		System.out.println("Rainbowtable in " + duration + "ms created");
@@ -23,7 +26,9 @@ public class Bonusaufgabe {
 		String[] hashesToCheck = { "29c3eea3f305d6b823f562ac4be35217", "12e2feb5a0feccf82a8d4172a3bd51c3",
 				"437988e45a53c01e54d21e5dc4ae658a", "c0e9a2f2ae2b9300b6f7ef3e63807e84", "1d56a37fb6b08aa709fe90e12ca59e12" };
 		for (int i = 0; i < hashesToCheck.length; i++) {
+			// Timer (in ms) to see how long the"hacking" of the hash took
 			start = System.currentTimeMillis();
+			//checking the given hashes, using the created rainbowtable and the hash- & reductionfunction
 			findPlaintext(hashesToCheck[i]);
 			duration = System.currentTimeMillis() - start;
 			System.out.println("Password in " + duration + "ms hacked");
@@ -31,6 +36,8 @@ public class Bonusaufgabe {
 		}
 	}
 
+	// creates the rainbowtable
+	// starts based on the calculated startValue
 	private static void createRainbowTable() {
 		for (int n = 0; n < N; n++) {
 			String startplaintext = calcStartValue(n);
@@ -46,6 +53,7 @@ public class Bonusaufgabe {
 		}
 	}
 
+	// calculates the startValue of the rainbowtable
 	private static String calcStartValue(int n) {
 		StringBuilder result = new StringBuilder();
 		for (int i = 1; i <= L; i++) {
@@ -55,6 +63,10 @@ public class Bonusaufgabe {
 		return result.toString();
 	}
 
+	// finds the plaintext
+	// iterates from a given hash to find the corresponding plaintext
+	// if the corresponding plaintext was found, prints the hash and the plaintext
+	// if the corresponding plaintext could not be found, prints a "password not found"-message
 	private static void findPlaintext(String hash) {
 		System.out.println("\nHash to search: " + hash);
 		for (int i = rainbowtable.size() - 1; i >= 0; i--) {
@@ -62,7 +74,7 @@ public class Bonusaufgabe {
 			String t = hash;
 			t = reductionfunction(t, j);
 
-			// go to end value
+			// go to end value, max 1999 steps
 			while (j < K - 1) {
 				j++;
 				t = hashfunction(t);
@@ -71,10 +83,13 @@ public class Bonusaufgabe {
 
 			if (rainbowtable.containsKey(t)) {
 				t = rainbowtable.get(t); // get start value
+				// hash & reduce the start value to the given hash
 				for (int k = 0; k < i; k++) {
 					t = hashfunction(t);
 					t = reductionfunction(t, k);
 				}
+				// compare given hash with hash of found plaintext
+				// if equal prints hash and plaintext
 				if (hashfunction(t).equals(hash)) {
 					System.out.println("Password to hash \"" + hash + "\" is: \"" + t + "\"");
 					return;
@@ -84,6 +99,7 @@ public class Bonusaufgabe {
 		System.out.println("Password not found!");
 	}
 
+	// hashfuntion used to hash the plaintext
 	private static String hashfunction(String plaintext) {
 		MessageDigest md;
 		try {
@@ -101,6 +117,7 @@ public class Bonusaufgabe {
 		return null;
 	}
 
+	// reductionfunction used to find the plaintext
 	// step: 0 <= x <= 1999
 	private static String reductionfunction(String hashtext, int step) {
 		BigInteger hash = new BigInteger(hashtext, 16);
