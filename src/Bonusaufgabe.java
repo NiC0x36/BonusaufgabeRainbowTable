4d34440db75408cfd04f34dfc3c77a3f4410f24f
Simon Rissi <simon.rissi@students.fhnw.ch>
2017-05-29 11:53:55 +0200

    MD5 simplified

--- src/Bonusaufgabe.java
+++ src/Bonusaufgabe.java
@@ -7,13 +7,15 @@ import java.security.*;
 public class Bonusaufgabe {
 	private static int N = 2000;
 	private static int K = 2000;
-	private static int L = 7; // Lï¿½nge des Passworts
+	private static int L = 7; // Länge des Passworts
 	private static char[] Z = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 	// k -> endpunkt / v -> startpunkt
 	private static Map<String, String> rainbowtable = new HashMap<String, String>();
+	private static MessageDigest md;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException {	
+		md = MessageDigest.getInstance("MD5");
 		System.out.println("Start");
 
 		// Timer (in ms) to see how long the creation of the rainbowtable took
@@ -31,7 +33,7 @@ public class Bonusaufgabe {
 			//checking the given hashes, using the created rainbowtable and the hash- & reductionfunction
 			findPlaintext(hashesToCheck[i]);
 			duration = System.currentTimeMillis() - start;
-			System.out.println("Password in " + duration + "ms hacked");
+			System.out.println("Password in " + duration + "ms hacked"); //TODO: output also when not found
 
 		}
 	}
@@ -43,9 +45,7 @@ public class Bonusaufgabe {
 			String startplaintext = calcStartValue(n);
 			String plaintext = startplaintext;
 			String hashtext;
-			for (int k = 0; k < K; k++) { // TODO: ACHTUNG! War "<=" so werden
-											// es jedoch 2001 berechnungen pro
-											// kette...
+			for (int k = 0; k < K; k++) {
 				hashtext = hashfunction(plaintext);
 				plaintext = reductionfunction(hashtext, k);
 			}
@@ -101,23 +101,11 @@ public class Bonusaufgabe {
 
 	// hashfuntion used to hash the plaintext
 	private static String hashfunction(String plaintext) {
-		MessageDigest md;
-		try {
-			md = java.security.MessageDigest.getInstance("MD5");
-			byte[] bytesOfMessage = md.digest(plaintext.getBytes());
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < bytesOfMessage.length; ++i) {
-				//TODO: anpassen
-				sb.append(Integer.toHexString((bytesOfMessage[i] & 0xFF) | 0x100).substring(1, 3)); // /TODO:
-			}
-			return sb.toString();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return null;
+		BigInteger bi = new BigInteger(1, md.digest(plaintext.getBytes()));
+		return bi.toString(16);
 	}
 
-	// reductionfunction used to find the plaintext
+	// reductionfunction used to calc the plaintext
 	// step: 0 <= x <= 1999
 	private static String reductionfunction(String hashtext, int step) {
 		BigInteger hash = new BigInteger(hashtext, 16);
